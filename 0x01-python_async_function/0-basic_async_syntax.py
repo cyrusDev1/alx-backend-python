a0392ce0b8bf73b32c68654c1e2a977ef8fd3989
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-01-13 14:30:57 +0100

    basic async syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+"""Write an asynchronous coroutine"""
 import random
 import asyncio
-"""Write an asynchronous coroutine"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
